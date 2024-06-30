3d4d3a73f62a7c24d4dd13bad71646cb844949ed
jovana0 <134726641+jovana0@users.noreply.github.com>
2024-06-30 19:12:32 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import BaseButton from "@/components/ui/BaseButton.vue";
-import dotenv from 'dotenv';
-dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
 const app = createApp(App);
 app.component("base-button", BaseButton);
 app.use(store);
