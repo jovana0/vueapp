6a99d8379e87aea5e950c96c8b6d92574a8f277a
jovana0 <134726641+jovana0@users.noreply.github.com>
2024-06-30 20:56:46 +0200

    Update register.php

--- server/register.php
+++ server/register.php
@@ -1,8 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit; // Handle preflight requests
+}
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $data = json_decode(file_get_contents("php://input"), true);
